11425fa38ccb70918a5503a99c738a8137055309
OSUKAL <floky798@gmail.com>
2021-12-25 20:17:53 +1000

    kekw

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,16 @@
 const mix = require('laravel-mix');
 
 mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css')
+    .setPublicPath('public')
+    .js('resources/assets/js/app.js', 'build/bundle.js')
+    .sass('resources/assets/scss/style.scss', 'build/bundle.css')
     .browserSync({
         watch: true,
         proxy: 'localhost',
-        browser: 'chrome',
         files: [
-            'resources/components/footer/**/*',
-            'resources/components/header/**/*',
-            'resources/components/**/*',
-            'resources/layouts/**/*',
-            'resources/routes/**/*',
-            'resources/views/**/*',
-            'resources/lang/**/*',
-            'resources/scss/**/*',
-            'resources/js/**/*',
-            'resources/**/*'
+            './public/**/*',
+            './resources/**/*.blade.php',
+            './resources/**/*.php',
+            './resources/**/*.twig'
         ]
     })
